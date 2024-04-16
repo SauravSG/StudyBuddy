550a5e49e1534220b5f91bd96cb83bf3ba389c67
SauravSG <sauravganc@gmail.com>
2024-04-16 23:08:37 +0530

    Updated again settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,11 +132,11 @@ USE_TZ = True
 # ]
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-  BASE_DIR / 'static',
-  BASE_DIR / 'staticfolder',
+  os.path.join(BASE_DIR, 'static'),
+  os.path.join(BASE_DIR, 'staticfolder'),
 ]
 
 
